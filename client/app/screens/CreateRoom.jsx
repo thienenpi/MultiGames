e0a611b5d20488d4d50cce9b72032341219b2e74
ThienNguyen <thienenpi280220@gmail.com>
2024-04-16 00:27:07 +0700

    fix: CreateRoom.jsx

--- client/app/screens/CreateRoom.jsx
+++ client/app/screens/CreateRoom.jsx
@@ -1,22 +1,24 @@
-import { Header } from 'react-native-elements';
-import { useNavigation } from '@react-navigation/native';
-
-// ...
+import { Header } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { View } from "react-native";
 
 const CreateRoom = () => {
   const navigation = useNavigation();
 
   return (
     <View>
-      {/* <Header
-        leftComponent={{ icon: 'arrow-back', color: '#fff', onPress: () => navigation.goBack() }}
-        centerComponent={{ text: 'Phòng Board Game', style: { color: '#fff' } }}
-        containerStyle={{
-          backgroundColor: '#3D6DCC',
-          justifyContent: 'space-around',
+      <Header
+        leftComponent={{
+          icon: "arrow-back",
+          color: "#fff",
+          onPress: () => navigation.goBack(),
         }}
-      /> */}
-     
+        centerComponent={{ text: "Phòng Board Game", style: { color: "#fff" } }}
+        containerStyle={{
+          backgroundColor: "#3D6DCC",
+          justifyContent: "space-around",
+        }}
+      />
     </View>
   );
 };
