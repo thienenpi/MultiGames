bfcce7a3f3e5e963b9f183aadb30649a97961637
Huynh Tien Phat <2409huynhphat@gmail.com>
2024-04-25 17:45:54 +0700

    refactor: appBar component

--- client/app/components/AppBar.jsx
+++ client/app/components/AppBar.jsx
@@ -4,28 +4,29 @@ import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 import { COLORS } from '../constants';
 import styles from './styles/appbar.style';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const AppBar = ({ title, showRightIcon, rightIconName, rightIconStyle, onPressLeftIcon, onPressRightIcon }) => {
   return (
-    <LinearGradient
-      colors={[COLORS.primaryDark, COLORS.primaryLight]}
-      style={styles.header}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 0 }}
-    >
-      <View style={styles.headerContent}>
-        <Pressable onPress={onPressLeftIcon}>
-          <Ionicons name="arrow-back" style={styles.iconLeft} />
-        </Pressable>
-        <Text style={styles.headerText}>{title}</Text>
-      </View>
-      
-      {showRightIcon && (
-        <Pressable onPress={onPressRightIcon}>
-          <Ionicons name={rightIconName} style={[styles.iconRight, rightIconStyle]} />
-        </Pressable>
-      )}
-    </LinearGradient>
+      <LinearGradient
+        style={styles.header}
+        colors={[COLORS.primaryDark, COLORS.primaryLight]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+      >
+        <View style={styles.headerContent}>
+          <Pressable onPress={onPressLeftIcon}>
+            <Ionicons name="arrow-back" style={styles.iconLeft} />
+          </Pressable>
+          <Text style={styles.headerText}>{title}</Text>
+        </View>
+
+        {showRightIcon && (
+          <Pressable onPress={onPressRightIcon}>
+            <Ionicons name={rightIconName} style={[styles.iconRight, rightIconStyle]} />
+          </Pressable>
+        )}
+      </LinearGradient>
   );
 };
 
