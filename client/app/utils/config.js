eeca838d53262f493cb7e86f16104344ba499cbf
Phat7203 <trantienphat.mx@gmail.com>
2024-05-22 16:24:35 +0700

    fix: rebuild android

--- client/app/utils/config.js
+++ client/app/utils/config.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "https://multigames.azurewebsites.net/api";
-const BASE_URL = "http://10.0.174.43:3000/api";
+const BASE_URL = "http://172.16.17.13:3000/api";
 
 export { BASE_URL };
